00f50773b059ddfae1a2d5565b96a84cb2a1d3f4
MoingXTwice <sckbs1314@gmail.com>
2022-03-23 15:50:32 +0900

    mongodb connect

--- schemas/index.js
+++ schemas/index.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
-
-const connect = () => {
-    mongoose.connect('mongodb://localhost:27017/homework', {ignoreUndefined: true}).catch((err) => {
-        console.error(err);
-    });
-};
-
-module.exports = connect;
+const mongoose = require("mongoose");const connect = () => {
+    mongoose
+        .connect("mongodb://localhost:27017/admin", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            ignoreUndefined: true,
+            user: "test",
+            pass: "test"
+        })
+        .catch(err => console.log(err));
+};mongoose.connection.on("error", err => {
+    console.error("몽고디비 연결 에러", err);
+});module.exports = connect;
